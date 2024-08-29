25e84ab38f463d1d8320a0919d501da70f5de081
MarcaoPeixoto <marco.peixoto@cubbo.com>
2024-08-29 11:51:59 -0300

    teste com loop e sleep no app
    tentativa de rodar os scripts sempre em loop

--- app.py
+++ app.py
@@ -2,31 +2,20 @@ from flask import Flask, render_template, send_from_directory, request, jsonify
 from flask_cors import CORS
 import json
 import os
-from apscheduler.schedulers.background import BackgroundScheduler
 import subprocess
+import time
+import threading
 
 app = Flask(__name__)
-CORS(app)  # Enable Cross-Origin Resource Sharing if needed
-scheduler = BackgroundScheduler()
+CORS(app)
 
-
-def job():
-    try:
+def run_scripts():
+    while True:
         subprocess.run(['python', 'incentivosExtrema.py'])
         subprocess.run(['python', 'incentivosEmbu.py'])
-        print("SLAs atualizados")
-    except subprocess.CalledProcessError as e:
-        print(f"An error occurred: {e}")
+        time.sleep(300)
 
-scheduler.add_job(job, 'interval', minutes=5)
-
-
-@app.before_request
-def start_scheduler():
-    if not scheduler.running:
-        scheduler.start()
-
-        
+      
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -128,6 +117,9 @@ def update_excluded_recibos():
         app.logger.error('Error updating excluded recibos JSON: %s', e)
         return jsonify(error=str(e)), 500
 
+def start_background_job():
+    threading.Thread(target=run_scripts, daemon=True).start()
+
 if __name__ == '__main__':
-    scheduler.start()
+    start_background_job() 
     app.run(host='0.0.0.0', debug=True)
